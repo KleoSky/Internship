922e2d4d54cdbd4717ad770793dd6b6459bd48bb
KleoSky <V-Kleo@yandex.ru>
2025-07-07 14:32:16 +0700

    Попытки публикации

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 // import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 /** @type {import('vite').UserConfig} */
@@ -65,7 +70,12 @@ export default {
   build: {
     outDir: '../dist',
   },
-  base: './',
+  base: '/accelerator-project-2/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'source'),
+    },
+  },
   server: {
     port: 3000,
   }
