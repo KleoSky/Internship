58c49744d5bc3a0573f593acc6daaafd69ac5c67
KleoSky <161670250+KleoSky@users.noreply.github.com>
2025-10-28 18:53:28 +0700

    Синхронизация (#14)
    * Начинает пагинацию news
    
    * Реализует пагинацию в news
    
    * Дорабатывает скрипт faq
    
    * Ezperiment
    
    * Корректирует скрипт аккордеона
    
    * Решает проблему переполнения hero
    
    * Дорабатывает валидацию, структуру по cms
    
    * Test
    
    * Test

--- source/js/modules/news.js
+++ source/js/modules/news.js
@@ -1,19 +1,34 @@
 import { Swiper as SwiperNews } from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 
 const initSwiperNews = () => {
   new SwiperNews('.news__wrapper-second', {
     loop: false,
-    modules: [Navigation],
+    modules: [Navigation, Pagination],
     navigation: {
       nextEl: '.news__pagination-arrow--next',
       prevEl: '.news__pagination-arrow--prev',
+    },
+    pagination: {
+      el: '.news__pagination-list',
+      clickable: true,
+      renderBullet: function (index) {
+        return `
+          <li class="swiper-pagination-bullet ${index === 0 ? 'swiper-pagination-bullet-current' : ''}" rel="tab-${index + 1}">
+            <button class="news__pagination-button ${index === 0 ? 'news__pagination-button--current' : ''}" type="button" rel="tab-${index + 1}">
+              ${index + 1}
+              <span class="visually-hidden">Новость ${index + 1}</span>
+            </button>
+          </li>
+        `;
+      },
     },
     breakpoints: {
       320: {
         slidesPerView: 1,
         direction: 'horizontal',
+        slidesOffsetAfter: 1,
       },
       768: {
         slidesPerView: 2,
@@ -28,13 +43,15 @@ const initSwiperNews = () => {
       },
     },
     on: {
-      init: function() {
+      init: function () {
         updateButtonStates(this);
         updateBigSlideClasses(this);
+        updatePaginationWindow(this);
       },
-      slideChange: function() {
+      slideChange: function () {
         updateButtonStates(this);
         updateBigSlideClasses(this);
+        updatePaginationWindow(this);
       }
     },
   });
@@ -67,6 +84,33 @@ const initSwiperNews = () => {
         }
       }
     }
+  }
+
+  function updatePaginationWindow(swiper) {
+    const paginationList = document.querySelector('.news__pagination-list');
+    const totalSlides = swiper.slides.length;
+    const activeIndex = swiper.activeIndex;
+    let startIdx = 0;
+    let endIdx = Math.min(3, totalSlides - 1);
+
+    if (totalSlides > 4) {
+      if (activeIndex >= 3) {
+        startIdx = Math.min(activeIndex - 2, totalSlides - 4);
+        endIdx = startIdx + 3;
+      }
+      if (activeIndex === totalSlides - 1) {
+        startIdx = totalSlides - 4;
+        endIdx = totalSlides - 1;
+      }
+    }
+
+    const bullets = paginationList.querySelectorAll('.swiper-pagination-bullet');
+    bullets.forEach((bullet, idx) => {
+      bullet.style.display = (idx >= startIdx && idx <= endIdx) ? 'block' : 'none';
+      bullet.classList.toggle('swiper-pagination-bullet-current', idx === activeIndex);
+      const bulletButton = bullet.querySelector('.news__pagination-button');
+      bulletButton?.classList.toggle('news__pagination-button--current', idx === activeIndex)
+    })
   }
 };
 
